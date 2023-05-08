741e928156bf88c9dc49b92747c991774c2bca19
Danny <dahneel@gmail.com>
2023-05-07 21:03:02 -0600

    added scope to manifest so service worker shows up

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,6 +42,7 @@ module.exports = () => {
       theme_color: '#225ca3',
       start_url: './',
       publicPath: './',
+      scope: './',
       icons: [
         {
           src: path.resolve('src/images/logo.png'),
